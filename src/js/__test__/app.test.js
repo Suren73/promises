6c6805f916728ce898b55ca993a68ce228880d94
Andrei Surkov <andronsurkov73@gmail.com>
2021-04-03 21:19:17 +0300

    Добавил выполненное Домашнее задание

---
+++ src/js/__test__/app.test.js
@@ -0,0 +1,7 @@
+import GameSavingLoader from '../app';
+
+test('testing the loading methode', async () => {
+  const expected = '{"id":9,"created":1546300800,"userInfo":{"id":1,"name":"Hitman","level":10,"points":2000}}';
+  const result = await GameSavingLoader.load().then((saving) => saving);
+  expect(result).toBe(expected);
+});
